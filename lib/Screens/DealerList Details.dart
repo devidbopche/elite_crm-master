a4d02603dd7ace063ec0c1f620638d8dcfd3db69
HP <devidbopche6@gmail.com>
2023-05-08 11:46:55 +0530

    https://github.com/devidbopche/elite_crm-master

--- lib/Screens/DealerList Details.dart
+++ lib/Screens/DealerList Details.dart
@@ -41,7 +41,7 @@ class DealerDetails extends StatefulWidget {
 
 class _DealerDetailsState extends State<DealerDetails> {
   Map<String, dynamic> dealerProfile = {};
-
+  var currentIndex = 0;
   Future<void> _getDealerProfile(String? dealerid) async {
     var body = {"dealerid": dealerid};
 
@@ -70,47 +70,107 @@ class _DealerDetailsState extends State<DealerDetails> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.all(20),
+        height: size.width * .155,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(.15),
+              blurRadius: 30,
+              offset: Offset(0, 10),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: ListView.builder(
+          itemCount: 4,
+          scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.symmetric(horizontal: size.width * .024),
+          itemBuilder: (context, index) => InkWell(
+            onTap: () {
+              setState(
+                    () {
+                  currentIndex = index;
+                },
+              );
+            },
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 1500),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  margin: EdgeInsets.only(
+                    bottom: index == currentIndex ? 0 : size.width * .029,
+                    right: size.width * .0422,
+                    left: size.width * .0422,
+                  ),
+                  width: size.width * .128,
+                  height: index == currentIndex ? size.width * .014 : 0,
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(10),
+                    ),
+                  ),
+                ),
+                Icon(
+                  listOfIcons[index],
+                  size: size.width * .076,
+                  color: index == currentIndex
+                      ? Colors.blueAccent
+                      : Colors.black38,
+                ),
+                SizedBox(height: size.width * .03),
+              ],
+            ),
+          ),
+        ),
+      ),
+
+
+
 
 
       backgroundColor: Color.fromARGB(255, 132, 134, 134),
-      bottomNavigationBar: Row(
-          children: [
-            InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const DealerList()),
-                  );
-                },
-                child: Container(
-                  padding: EdgeInsets.all(15),
-                  width: 100.w,
-                  height: 5.8.h,
-                  color: ColorConstants.deppp,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // Text("Continue "),
-                      Icon(
-                        Icons.arrow_back,
-                        color: Colors.black,
-                        size:20.sp,
-                      ),
-                      Text( " Go Back", style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w900
-                      ),)
-                    ],
-                  ),
-                )),
 
-
-
-
-
-
-          ]),
+      // bottomNavigationBar: Row(
+      //     children: [
+      //       InkWell(
+      //           onTap: () {
+      //             Navigator.push(
+      //               context,
+      //               MaterialPageRoute(builder: (context) => const DealerList()),
+      //             );
+      //           },
+      //           child: Container(
+      //             padding: EdgeInsets.all(15),
+      //             width: 100.w,
+      //             height: 5.8.h,
+      //             color: ColorConstants.deppp,
+      //             child: Row(
+      //               mainAxisAlignment: MainAxisAlignment.center,
+      //               children: [
+      //                 // Text("Continue "),
+      //                 Icon(
+      //                   Icons.arrow_back,
+      //                   color: Colors.black,
+      //                   size:20.sp,
+      //                 ),
+      //                 Text( " Go Back", style: TextStyle(
+      //                     fontSize: 15,
+      //                     fontWeight: FontWeight.w900
+      //                 ),)
+      //               ],
+      //             ),
+      //           )),
+      //     ]),
       appBar: AppBar(
         title: Text('Dealer List'),
         actions: [
@@ -236,8 +296,8 @@ class _DealerDetailsState extends State<DealerDetails> {
                 children:<Widget> [
                   ///  call  buttton
                   Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: new BoxDecoration(
+                      padding: const EdgeInsets.all(10),
+                      decoration:  const BoxDecoration(
                           shape: BoxShape.circle,
                           gradient: LinearGradient(
                               begin: Alignment.bottomCenter,
@@ -250,7 +310,7 @@ class _DealerDetailsState extends State<DealerDetails> {
                         onPressed: () async {
                           _launchPhoneURL(dealerProfile['direct']);
                         },
-                        child: new Icon(
+                        child:  Icon(
                           Icons.call,
                           color: Colors.grey[600],
                           size: 28.0,
@@ -266,8 +326,8 @@ class _DealerDetailsState extends State<DealerDetails> {
 
                   ///  gmail_buttton
                   Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: new BoxDecoration(
+                      padding: const EdgeInsets.all(10),
+                      decoration: const BoxDecoration(
                           shape: BoxShape.circle,
                           gradient: LinearGradient(
                               begin: Alignment.bottomCenter,
@@ -288,7 +348,7 @@ class _DealerDetailsState extends State<DealerDetails> {
                             //email app is not opened
                           }
                         },
-                        child: new Icon(
+                        child:  Icon(
                           Icons.mail,
                           color: Colors.grey[600],
                           size: 28.0,
@@ -362,6 +422,12 @@ class _DealerDetailsState extends State<DealerDetails> {
       throw 'Could not launch $url';
     }
   }
+  List<IconData> listOfIcons = [
+    Icons.home_rounded,
+    Icons.favorite_rounded,
+    Icons.settings_rounded,
+    Icons.person_rounded,
+  ];
 }
 
 
